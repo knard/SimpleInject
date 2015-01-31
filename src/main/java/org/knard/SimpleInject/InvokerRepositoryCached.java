1ce7cddeebea2442f4cc5abe59bdc2c77e0970a3
knard <knard@knard.org>
2015-01-31 22:33:55 +0100

    fix the cache system.
    Original implementation was design to work with a limited size but I was
    to tired to design it correctly. cache size seems to not be need for
    now, so I'll remove it.

--- src/main/java/org/knard/SimpleInject/InvokerRepositoryCached.java
+++ src/main/java/org/knard/SimpleInject/InvokerRepositoryCached.java
@@ -20,33 +20,24 @@
 package org.knard.SimpleInject;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class InvokerRepositoryCached extends InvokerRepository {
 
-	private final LinkedList<Class<?>> cacheBasket;
 	private final Map<Class<?>, Invoker> invokerMap;
-	private final int cacheSize;
 
-	public InvokerRepositoryCached(final int cacheSize) {
-		this.cacheSize = cacheSize;
-		this.cacheBasket = new LinkedList<Class<?>>();
-		this.invokerMap = new HashMap<Class<?>, Invoker>(cacheSize + 1);
+	public InvokerRepositoryCached() {
+		this.invokerMap = new HashMap<Class<?>, Invoker>();
 	}
 
 	@Override
-	public Invoker getInvoker(final Class<?> c) {
+	public synchronized Invoker getInvoker(final Class<?> c) {
 		Invoker invoker = this.invokerMap.get(c);
 		if (invoker != null) {
 			return invoker;
 		}
 		invoker = super.getInvoker(c);
 		this.invokerMap.put(c, invoker);
-		this.cacheBasket.addFirst(c);
-		if (this.cacheBasket.size() > this.cacheSize) {
-			this.cacheBasket.removeLast();
-		}
 		return invoker;
 	}
 
